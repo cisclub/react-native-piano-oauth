e24d9af8087788dc93e9d0606997344f67061f2a
aliamin <aamin@gulfnews.com>
2020-09-16 10:28:53 +0400

    add methods to index.js

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 import { NativeModules } from 'react-native';
 
-const { PianoOauth } = NativeModules;
+const PianoOauthModule = NativeModules.PianoOAuth;
+
+PianoOauth = {
+    /*
+     * @param aid                               set this value to <PUBLISHER_AID>
+     * @param endpointURL
+     * @param signInCallbackHandler             callback handler called after user sign in.
+     * @param didCancelSignInCallbackHandler    callback handler called when user cancel sign in.
+     */
+    signInWithAID(aid: String, endpointURL: String, didSignInCallbackHandler = ({token, error}) => {}, didCancelSignInCallbackHandler = () => {}) {
+        PianoOauthModule.signInWithAID(
+            aid,
+            endpointURL,
+            signInCallbackHandler,
+            didCancelSignInCallbackHandler);
+    },
+
+    /*
+     * @param token                     token of user to sign out.
+     * @param signOutCallbackHandler    callback handler called after sign out.
+     */
+    signOutWithToken(token: String, signOutCallbackHandler = (error) => {}) {
+        PianoOauthModule.signOutWithToken(
+            token,
+            signOutCallbackHandler);
+    }
+};
 
 export default PianoOauth;
